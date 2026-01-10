3ee8ad877265df9d82ec221d72f6783430129830
rekha kumawat <rekhasumanku@gmail.com>
2026-01-10 12:35:37 -0500

    Add files via upload

---
+++ Classwork/Day_4/Lambda1.py
@@ -0,0 +1,9 @@
+# functional
+# lambda function use (special function)
+
+Add = lambda no1 , no2: no1+ no2
+
+Ret = Add(11,10)
+# Ret = 11 +10                  internally it changes as
+
+print("Addition is :",Ret)  
---
+++ Classwork/Day_4/Lambda2.py
@@ -0,0 +1,24 @@
+
+# functional approach (lambda function)
+# check even odd
+
+
+CheckEven = lambda No : No % 2 == 0
+
+
+def main():
+
+    Value = 0
+    Ret = False
+    print("Enter a Number:-")
+    Value = int(input())
+    Ret = CheckEven(Value)   
+
+    if(Ret == True):
+        print("Number is Even")
+
+    else:
+        print("Number is Odd")                                          
+
+if __name__ == "__main__":
+    main()
